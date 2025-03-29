936dd8c2548b683ec634e864dcc2198c073f266e
Evgeny Simonov <jdsimonov2@gmail.com>
2025-03-29 14:05:20 +0300

    modified:   src/services/ServiceManager.ts

--- src/services/ServiceManager.ts
+++ src/services/ServiceManager.ts
@@ -1,15 +1,16 @@
-import { Api } from "../components/base/api";
+import { WebLarekApi } from "../components/WebLarekAPI";
 import { EventBroker } from "../components/base/events";
 import { DataRepository } from "./DataRepository";
+import {API_URL, CDN_URL} from "../utils/constants";
 
 export class ServiceManager {
   private static instance: ServiceManager;
-  public api: Api;
+  public api: WebLarekApi;
   public eventBroker: EventBroker;
   public dataRepo: DataRepository;
 
   private constructor() {
-    this.api = new Api();
+    this.api = new WebLarekApi(CDN_URL, API_URL);
     this.eventBroker = new EventBroker();
     this.dataRepo = DataRepository.getInstance(this.api);
   }
