1a3bb411f8364a6b73faefd863dee8df250b82ea
Evgeny Simonov <jdsimonov2@gmail.com>
2025-03-29 14:00:18 +0300

    new file:   src/components/WebLarekAPI.ts

---
+++ src/components/WebLarekAPI.ts
@@ -0,0 +1,30 @@
+import { Api, ApiListResponse } from "./base/api";
+import { IProduct } from "../types";
+import {CDN_URL, API_URL} from '../utils/constants'
+export interface IWebLarekApi {
+    getCards: () => Promise<IProduct[]>;
+  }
+  
+  export class WebLarekApi extends Api implements IWebLarekApi {
+    readonly cdn: string;
+    constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+      super(baseUrl, options);
+      this.cdn = cdn;
+    }
+  
+    getCards(): Promise<IProduct[]> {
+      return this.get('/product').then((data: ApiListResponse<IProduct>) =>
+        data.items.map((item) => ({
+            ...item,
+            image: this.cdn + item.image
+        }))
+      );
+    }
+  }
+  
+  const api = new WebLarekApi(CDN_URL, API_URL);
+  
+  api.getCards()
+    .then((res) => {
+      console.log(res);
+    });
