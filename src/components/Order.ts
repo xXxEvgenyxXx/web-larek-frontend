38fdad96ab71f5e1c6594923e8d03f6e5d3af6df
Evgeny Simonov <jdsimonov2@gmail.com>
2025-07-10 21:58:35 +0300

    modified:   src/components/Order.ts

--- src/components/Order.ts
+++ src/components/Order.ts
@@ -6,12 +6,4 @@ export class Order extends Form<IOrderForm> {
     constructor(container: HTMLFormElement, events: IEvents) {
         super(container, events);
     }
-
-    //set phone(value: string) {
-    //    (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
-    //}
-
-    //set email(value: string) {
-    //    (this.container.elements.namedItem('email') as HTMLInputElement).value = value;
-    //}
 }
