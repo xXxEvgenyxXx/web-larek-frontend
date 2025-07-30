5e203daf3bdaca872cfc80524a281f6d40932a54
Evgeny Simonov <jdsimonov2@gmail.com>
2025-07-30 21:05:59 +0300

    modified:   src/components/Basket.ts

--- src/components/Basket.ts
+++ src/components/Basket.ts
@@ -1,5 +1,5 @@
 import { Component } from "./base/Component";
-import {createElement, ensureElement, formatNumber} from "./../utils/utils";
+import { createElement, ensureElement, formatNumber } from "../utils/utils";
 import { EventEmitter } from "./base/events";
 
 interface IBasketView {
@@ -10,21 +10,21 @@ interface IBasketView {
 export class Basket extends Component<IBasketView> {
     protected _list: HTMLElement;
     protected _total: HTMLElement;
-    protected _button: HTMLElement;
+    protected _button: HTMLButtonElement; // Изменено на HTMLButtonElement
 
     constructor(container: HTMLElement, protected events: EventEmitter) {
         super(container);
 
         this._list = ensureElement<HTMLElement>('.basket__list', this.container);
         this._total = ensureElement<HTMLElement>('.basket__price', this.container);
-        this._button = this.container.querySelector('.basket__button');
+        this._button = ensureElement<HTMLButtonElement>('.basket__button', this.container);
 
-        if (this._button) {
-            this._button.addEventListener('click', () => {
-                events.emit('order:open');
-            });
-        }
+        // Добавляем обработчик клика
+        this._button.addEventListener('click', () => {
+            events.emit('order:open');
+        });
 
+        // Инициализируем пустой список
         this.items = [];
     }
 
@@ -32,10 +32,14 @@ export class Basket extends Component<IBasketView> {
         if (items.length) {
             this._list.replaceChildren(...items);
         } else {
-            this._list.replaceChildren(createElement<HTMLParagraphElement>('p', {
-                textContent: 'Корзина пуста'
-            }));
+            this._list.replaceChildren(
+                createElement<HTMLParagraphElement>('p', {
+                    textContent: 'Корзина пуста'
+                })
+            );
         }
+        // Обновляем состояние кнопки
+        this._button.disabled = items.length === 0;
     }
 
     set total(total: number) {
